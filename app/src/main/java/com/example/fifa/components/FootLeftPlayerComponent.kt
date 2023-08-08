7a32e8906751dddd44fbd27485d5efc3c98a1748
Alejandro Gómez Ochoa de Retana <agomezo456@gmail.com>
2023-08-08 13:02:14 +0200

    FootComponent

---
+++ app/src/main/java/com/example/fifa/components/FootLeftPlayerComponent.kt
@@ -0,0 +1,56 @@
+package com.example.fifa.components
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.ImageView
+import com.example.fifa.R
+
+class FootLeftPlayerComponent @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val imageView: ImageView
+
+    var myFoot: String = "Left"
+        set(value) {
+            field = value
+            selectImage()
+        }
+
+/*
+    fun setMyFoot(foot: String) {
+        myFoot = foot
+        selectImage()
+    }*/
+
+    init {
+        imageView = ImageView(context)
+        addView(imageView)
+
+        /*Log.w("ZURDOODIESTRO", "El jugador es $myFoot")
+        imageView = if (myFoot == "Right") {
+            inflate(context, R.layout.diestro, this)
+                .findViewById(R.id.diestro)
+        } else{
+            inflate(context, R.layout.zurdo, this)
+                .findViewById(R.id.zurdo)
+        }*/
+
+    }
+
+    @SuppressLint("ResourceType")
+    private fun selectImage() {
+        imageView.setImageResource(
+            if (myFoot == "Left") {
+                R.drawable.zurdoverde
+            } else {
+                R.drawable.zurdonegro
+            }
+        )
+    }
+
+}
--- app/src/main/java/com/example/fifa/presentation/detail/PlayerDetailScreen.kt
+++ app/src/main/java/com/example/fifa/presentation/detail/PlayerDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.example.fifa.presentation.detail
 
 import android.annotation.SuppressLint
-import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -15,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -26,24 +26,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.fifa.R
+import com.example.fifa.components.FootLeftPlayerComponent
+import com.example.fifa.components.FootRightPlayerComponent
 import com.example.fifa.data.remote.TOKEN
-import com.example.fifa.domain.model.PlayerModel
-import com.example.fifa.navigation.Screen
-import com.example.fifa.ui.theme.globalPadding
 import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -61,13 +60,13 @@ fun PlayerDetailScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    myPlayer?.name?.let { 
+                    myPlayer?.name?.let {
                         Text(
                             text = it,
                             color = Color.White
                         )
                     }
-                    
+
                 },
                 navigationIcon = {
                     IconButton(
@@ -165,7 +164,7 @@ fun PlayerDetailScreen(
                             textAlign = TextAlign.Center,
                         )
                     }
-                    Spacer(
+/*                    Spacer(
                         modifier = Modifier
                             .size(8.dp)
                     )
@@ -178,6 +177,47 @@ fun PlayerDetailScreen(
                             text = "${myPlayer?.foot}",
                             textAlign = TextAlign.Center,
                         )
+                    }*/
+
+                    Spacer(
+                        modifier = Modifier
+                            .size(8.dp)
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .background(Color.White, CircleShape)
+                            .border(1.dp, Color.Black, CircleShape),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+
+                            AndroidView(
+                                modifier = Modifier
+                                    .size(40.dp),
+                                factory = { context ->
+                                    FootLeftPlayerComponent(context).apply {
+                                        this.myFoot = myPlayer?.foot.toString()
+                                    }
+                                },
+                                update = {
+                                    it.myFoot = myPlayer?.foot.toString()
+                                }
+                            )
+                            AndroidView(
+                                modifier = Modifier
+                                    .size(40.dp),
+                                factory = { context ->
+                                    FootRightPlayerComponent(context).apply {
+                                        this.myFoot = myPlayer?.foot.toString()
+                                    }
+                                },
+                                update = {
+                                    it.myFoot = myPlayer?.foot.toString()
+                                }
+                            )
+
+                        //PIES
                     }
                 }
             }
